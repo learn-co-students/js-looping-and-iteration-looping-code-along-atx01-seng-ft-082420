a8177014b97db9abba593fa3f9f5eb26042008c8
whychooseaname <edward.ea.acosta@gmail.com>
2020-10-05 19:10:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let thanks=[]
+    for( let i=0; i<names.length; i++){
+    thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanks
+}
+
+function countDown(number){
+    while( number > -1 ){
+        console.log(number);
+        number -= 1;
+    }
+    // for (let number = number; number > -1; number--){
+    //     console.log(number)
+    // }
+}
